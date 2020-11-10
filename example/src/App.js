667aa8a8507f35c1790623ed6e6564ae6e738e9d
Jeffrey Pinyan <jeffrey.pinyan@ithreat.com>
2020-11-10 09:27:47 -0500

    init create-react-library@3.1.1

---
+++ example/src/App.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+import { ExampleComponent } from '@ithreat/toast-provider'
+import '@ithreat/toast-provider/dist/index.css'
+
+const App = () => {
+  return <ExampleComponent text="Create React Library Example 😄" />
+}
+
+export default App
